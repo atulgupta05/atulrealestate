6ba4a2f896ed294f04d723cafc5cd278ee30d0be
Atul Gupta <atulajitdewantigupta@gmail.com>
2024-05-06 15:58:05 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import './Header.css'
+import { BiMenuAltRight } from 'react-icons/bi'
+import OutsideClickHandler from 'react-outside-click-handler'
+
+
+const Header = () => {
+    const [menuOpened, setMenuOpened] = useState(false)
+
+    const getMenyStyles = (menuOpened) => {
+        if (document.documentElement.clientWidth <= 800) {
+            return { right: !menuOpened && "-100%" }
+        }
+    }
+    return (
+        <section className="h-wrapper">
+            <div className="flexCenter paddings innerWidth h-container">
+                <img src="./logo.png" alt="logo" width={100} />
+                <OutsideClickHandler onOutsideClick={()=> {setMenuOpened(false)}}>
+
+                    <div className="flexCenter h-menu" style={getMenyStyles(menuOpened)}>
+                        <a href="">Residencies</a>
+                        <a href="">Our Values</a>
+                        <a href="">Contact Us</a>
+                        <a href="">Get Started</a>
+                        <button className='button'><a href="">Contact</a></button>
+                    </div>
+
+
+                
+                <div className="menu-icon" onClick={() => setMenuOpened((prev) => !prev)}>
+                    <BiMenuAltRight size={30} />
+                </div>
+                </OutsideClickHandler>
+            </div>
+
+        </section>
+    )
+}
+
+export default Header
